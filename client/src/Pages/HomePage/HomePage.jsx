9ac1fe2f7af06d82e651c7a881e51347cd3f1222
Frederick Hazel <frederick_hazel_hotmail.com>
2020-11-24 18:45:47 +0000

    login done

--- client/src/Pages/HomePage/HomePage.jsx
+++ client/src/Pages/HomePage/HomePage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { Container, Row, Col, Button, Form } from "react-bootstrap";
 
 import { useForm } from "react-hook-form";
 import { Notyf } from "notyf";
@@ -20,35 +20,98 @@ function HomePage({ history }) {
         history.push(`/chat?name=${data.username}&room=${data.room}`);
     };
     return (
-        <div className="main">
+        <div className=" home-background">
             <Container>
                 <Row>
-                    <Col xs={12} xl={12}>
-                        <form onSubmit={handleSubmit(submitHandler)}>
-                            <div className="Input">
-                                <input
-                                    type="text"
-                                    id="input"
-                                    name="username"
-                                    className="form__field"
-                                    placeholder="Enter Username"
-                                    ref={register({
-                                        required: true,
-                                    })}
-                                />
-                                <input
-                                    type="text"
-                                    id="input"
-                                    name="room"
-                                    className="form__field"
-                                    placeholder="Enter Room Name"
-                                    ref={register({
-                                        required: true,
-                                    })}
-                                />
-                                <button>Join</button>
-                            </div>
-                        </form>
+                    <Col xs={12} sm={12} md={{ span: 10, offset: 1 }} lg={{ span: 11, offset: 1 }} xl={{ span: 11, offset: 1 }}>
+                        <div className="login-card shadow rounded">
+                            <Row>
+                                <Col xs={0} sm={0} md={0} lg={6} xl={6}>
+                                    <div className="login-image"></div>
+                                </Col>
+                                <Col xs={12} sm={12} md={12} lg={6} xl={6}>
+                                    <div>
+                                        <h4 className="text-center mt-5 brim-text">Brim Portal</h4>
+
+                                        <Form onSubmit={handleSubmit(submitHandler)}>
+                                            <Row>
+                                                <Col
+                                                    xs={{ offset: 1, span: 10 }}
+                                                    sm={{ offset: 1, span: 10 }}
+                                                    md={{ offset: 1, span: 10 }}
+                                                    lg={10}
+                                                    xl={10}
+                                                >
+                                                    {" "}
+                                                    <h5 className="text-center brim-text">Enter Your Brimtails</h5>
+                                                    <hr />
+                                                </Col>
+                                            </Row>
+                                            <Row>
+                                                <Col
+                                                    xs={{ offset: 1, span: 10 }}
+                                                    sm={{ offset: 1, span: 10 }}
+                                                    md={{ offset: 1, span: 10 }}
+                                                    lg={10}
+                                                    xl={10}
+                                                >
+                                                    {" "}
+                                                    <Form.Group controlId="formBasicid">
+                                                        <Form.Label>Enter Username</Form.Label>
+                                                        <Form.Control
+                                                            size="lg"
+                                                            type="text"
+                                                            placeholder="Username"
+                                                            name="username"
+                                                            ref={register({
+                                                                required: true,
+                                                            })}
+                                                        />
+                                                    </Form.Group>
+                                                </Col>
+                                            </Row>
+                                            <Row>
+                                                <Col
+                                                    xs={{ offset: 1, span: 10 }}
+                                                    sm={{ offset: 1, span: 10 }}
+                                                    md={{ offset: 1, span: 10 }}
+                                                    lg={10}
+                                                    xl={10}
+                                                >
+                                                    <Form.Group controlId="formBasicPassword">
+                                                        <Form.Label>Enter Room Name</Form.Label>
+                                                        <Form.Control
+                                                            size="lg"
+                                                            type="text"
+                                                            placeholder="Room Name"
+                                                            name="room"
+                                                            ref={register({
+                                                                required: true,
+                                                            })}
+                                                        />
+                                                    </Form.Group>
+                                                </Col>
+                                            </Row>
+
+                                            <Row>
+                                                {" "}
+                                                <Col
+                                                    xs={{ offset: 1, span: 10 }}
+                                                    sm={{ offset: 1, span: 10 }}
+                                                    md={{ offset: 1, span: 10 }}
+                                                    lg={10}
+                                                    xl={10}
+                                                >
+                                                    <Button type="submit" size="lg" className="login-button mb-5">
+                                                        Join
+                                                    </Button>
+                                                </Col>
+                                            </Row>
+                                        </Form>
+                                    </div>
+                                </Col>
+                            </Row>
+                        </div>
                     </Col>
                 </Row>
             </Container>
