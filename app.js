03f4cb3c53c3f7e18c23b7363a39b9715cd47a8b
Frederick Hazel <frederick_hazel_hotmail.com>
2020-11-26 18:31:31 +0000

    changes

--- app.js
+++ app.js
@@ -105,6 +105,16 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    });
+}
+if (process.env.NODE_ENV === "development") {
+    app.use(express.static(path.join(__dirname, "public")));
+}
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
